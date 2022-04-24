d3c6bef0cc23e91148b3fcec9aff637526317a18
Umer Asghar <omar.asghar@yahoo.com>
2022-04-24 18:21:49 +0200

    updated the app.py file for comments

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 from bbquote.lib import get_quote
 
+#These are the quotes with the show name
+
 quote = get_quote()  # assuming the function returns an author and a quote
 
 f"{quote}"
